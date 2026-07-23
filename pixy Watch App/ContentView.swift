186f850c506e3d5efc87d18140a11620e441bce7
bitanath <bitanath@gmail.com>
2026-07-23 07:54:55 +0530

    Initial Commit

---
+++ pixy Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pixy Watch App
+//
+//  Created by Bitan Nath on 23/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pixy Watch App/pixyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixyApp.swift
+//  pixy Watch App
+//
+//  Created by Bitan Nath on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct pixy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
